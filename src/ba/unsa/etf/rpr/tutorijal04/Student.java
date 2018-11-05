f3be596371845f398af04da14d6686ca3d82b0e5
yaly1 <yaly1@etf.unsa.ba>
2018-11-05 20:51:24 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/tutorijal04/Student.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.tutorijal04;
+
+public class Student {
+    private String ime, prezime;
+    private int brojSemestra = 1;
+    //private Predmet[] trenutniPredmeti;
+    private boolean jeliPoPlanu = false;
+
+    public Student(String ime, String prezime, int brojSemestra) {}
+
+    //public void dodajPredmet(Predmet predmet) {}
+
+    //public void brisiPredmet(Predmet predmet) {}
+
+    public void idiNaSljedeciSemestar() {}
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
